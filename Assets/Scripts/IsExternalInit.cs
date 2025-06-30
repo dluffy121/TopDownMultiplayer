345ccc19aef5a04e9bffef1fc14df1d47d43d41b
dluffy121 <waghela.sonic@gmail.com>
2025-06-30 23:21:52 +0530

    - enabled c# 9 init-only properties for record usage

---
+++ Assets/Scripts/IsExternalInit.cs
@@ -0,0 +1,9 @@
+namespace System.Runtime.CompilerServices
+{
+    public class IsExternalInit
+    {
+        // This class is used to support C# 9.0's init-only properties in Unity, which does not support it natively.
+        // It is a workaround to allow init-only properties to be used in Unity scripts.
+        // The class itself does not contain any members or methods.
+    }
+}
