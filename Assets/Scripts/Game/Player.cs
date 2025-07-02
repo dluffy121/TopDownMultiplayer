c92b35b25bc3da07f77f8d42189245b7d5122711
dluffy121 <waghela.sonic@gmail.com>
2025-07-03 01:35:32 +0530

    - updated Player, using pickup to assign current weapon

--- Assets/Scripts/Game/Player.cs
+++ Assets/Scripts/Game/Player.cs
@@ -6,17 +6,21 @@ namespace TDM
     public class Player : NetworkBehaviour, INetworkInputListener
     {
         [SerializeField] private NetworkCharacterController _charController;
-
         [SerializeField] private float _speed;
-
         [SerializeField] private ProjectileSpawner _bulletSpawner;
-        [SerializeField] private HitscanWeapon _weapon;
 
+        [Header("Visual")]
+        [SerializeField] private Material _localMaterial;
+        [SerializeField] private Renderer _renderer;
+
+        private IWeapon[] _weapons;
         private Vector3 _forward = Vector3.forward;
 
         void Awake()
         {
             _forward = transform.forward;
+
+            _weapons = GetComponentsInChildren<IWeapon>(true);
         }
 
         void OnEnable()
@@ -27,6 +31,14 @@ namespace TDM
         void OnDisable()
         {
             NetworkManager.UnregisterFromInput(this);
+        }
+
+        public override void Spawned()
+        {
+            base.Spawned();
+
+            if (Runner.LocalPlayer == Object.InputAuthority)
+                _renderer.material = _localMaterial;
         }
 
         public override void FixedUpdateNetwork()
@@ -42,20 +54,17 @@ namespace TDM
             if (inputData.direction.sqrMagnitude > 0)
                 _forward = inputData.direction;
 
-            // if (HasStateAuthority
-            //     && _bulletSpawner.CanSpawnProjectile(Runner, Object.InputAuthority, inputData))
-            // {
-            //     _bulletSpawner.SpawnProjectile<SimpleBullet>(Runner,
-            //                                                  Object.InputAuthority,
-            //                                                  transform.position + _forward,
-            //                                                  Quaternion.LookRotation(_forward));
-            // }
+            if (HasInputAuthority)
+                FireWeapon(inputData);
+        }
 
-            if (HasStateAuthority
-                && _weapon.CanSpawnProjectile(Runner, Object.InputAuthority, inputData))
-            {
-                _weapon.Fire(Runner);
-            }
+        void OnTriggerEnter(Collider other)
+        {
+            if (!Object.HasInputAuthority) return;
+
+            if (other.CompareTag("Pickup")
+                && other.TryGetComponent(out Pickup pickup))
+                Rpc_RequestPickup(pickup.Object.Id);
         }
 
         #region Input
@@ -73,6 +82,45 @@ namespace TDM
 
             if (Input.GetKey(KeyCode.D))
                 input.direction += Vector3.right;
+        }
+
+        #endregion
+
+        #region Pickup
+
+        [Rpc(RpcSources.InputAuthority, RpcTargets.StateAuthority)]
+        internal void Rpc_RequestPickup(NetworkId pickupId, RpcInfo info = default)
+        {
+            NetworkObject networkObject = Runner.FindObject(pickupId);
+            if (networkObject == null) return;
+            if (!networkObject.TryGetComponent(out Pickup pickup)) return;
+            if (!pickup.TryGetWeapon(out int weaponIndex)) return;
+
+            RPC_AssignWeapon(weaponIndex);
+        }
+
+        #endregion
+
+        #region Weapon
+
+        [Networked] int WeaponIndex { get; set; } = -1;
+
+        private void FireWeapon(NPlayerInputData inputData)
+        {
+            if (WeaponIndex > 0
+                && _weapons[WeaponIndex].CanSpawnProjectile(Runner, Object.InputAuthority, inputData))
+                _weapons[WeaponIndex].Fire(Runner);
+        }
+
+        [Rpc(RpcSources.StateAuthority, RpcTargets.All)]
+        internal void RPC_AssignWeapon(int weaponIndex)
+        {
+            if (WeaponIndex > 0)
+                _weapons[WeaponIndex].gameObject.SetActive(false);
+
+            WeaponIndex = weaponIndex;
+
+            _weapons[WeaponIndex].gameObject.SetActive(true);
         }
 
         #endregion
